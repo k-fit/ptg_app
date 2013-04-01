3838c7071e4758fd85cbe2a3b2a7396041bbb18f
Matt DeLand <mdeland@groupon.com>
2013-03-31 19:26:49 -0700

    minor changes

--- app/models/localtrade.rb
+++
@@ -1,5 +0,0 @@
-class Localtrade < ActiveRecord::Base
-  attr_accessible :broker, :buy, :content_type, :price, :settlement_period, :trader, :volume
-  belongs_to :user
-  validates :price, :numericality => true
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
   			uniqueness: { case_sensitive: false }
-
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 end
